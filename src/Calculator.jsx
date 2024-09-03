1b6a54ac75e22afd2ecbbb8d1bcd45c1a5e0dbc5
Jamil115 <jamil71001@gmail.com>
2024-09-03 12:49:34 +0600

    design changed

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -264,71 +264,67 @@ export default function Calculator() {
 
     return (
         <div className='bg-gradient-to-r from-[#614385] to-[#516395] h-screen flex justify-center items-center'>
-            <table className='border-[15px] border-red-300'>
-                    <thead className='border-7 bg-white'>
-                        <tr>
-                            <th colSpan={4}>
-                                <div type="text" className='bg-[#00342f] m-1 h-20 w-[230px] border-2 flex flex-col justify-end'>
-                                    <div className='text-slate-400 text-right font-semibold text-xl mb-1'>
-                                        {
-                                            (show != '')? 
-                                            <p>{show}=</p> 
-                                            : ''
-                                        }
-                                    </div>
-                                    <div className='text-white text-right font-semibold text-xl mb-1 overflow-hidden'>
-                                        {
-                                            (data == input)? input : data
-                                        }
-                                    </div> 
-                                </div>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody className='bg-[#bff1f0] h-72 font-semibold text-lg'>
-                        <tr className='text-center'>
-                            <td onClick={()=>{setData(""); setInput(0); setShow('')}}  className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] w-16'>AC</td>
-                            <td onClick={()=>setData(data.slice(0,-1))} className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] w-16'>Del</td>
-                            <td onClick={(event)=>setData(data + 3.1415926535897932384626433832795) } className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] w-16'>&#8508;</td>
-                            <td onClick={getFactorial} className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] w-16'>n!</td>
-                        </tr>
+            <div className='bg-slate-300 border-collapse border-[15px] border-slate-300 rounded-2xl shadow-2xl'>
+                    <div className='mb-3 bg-slate-300'>
+                        <div type="text" className='bg-[#00342f] h-28 w-[320px] border-2 flex flex-col justify-end font-["./font/DSEG14Modern-RegularItalic_t7zez4.ttf"]'>
+                            <div className='text-slate-400 text-right font-semibold text-xl mb-2'>
+                                {
+                                    (show != '')? 
+                                    <p>{show}=</p> 
+                                    : ''
+                                }
+                            </div>
+                            <div className='text-white text-right font-semibold text-4xl mb-3 overflow-hidden'>
+                                {
+                                    (data == input)? input : data
+                                }
+                            </div> 
+                        </div>
+                    </div>
+                    <div className='h-[440px] w-80 font-semibold text-lg'>
+                        <div className=''>
+                            <button onClick={()=>{setData(""); setInput(0); setShow('')}}  className='bg-[#efa98f] hover:bg-[#e89c80] m-2 h-14 cursor-pointer border-2 border-[#e1feff] rounded-xl w-16'>AC</button>
+                            <button onClick={()=>setData(data.slice(0,-1))} className='bg-[#efa98f] hover:bg-[#e89c80] m-2 h-14 cursor-pointer border-2 border-[#e1feff] rounded-xl w-16'>Del</button>
+                            <button onClick={(event)=>setData(data + 3.1415926535897932384626433832795) } className='m-2 h-14 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl w-16'>&#8508;</button>
+                            <button onClick={getFactorial} className='m-2 h-14 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl w-16'>n!</button>
+                        </div>
 
-                        <tr className='text-center'>
-                            <td onClick={(event)=>setData(data + event.target.innerText) }  className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] w-16'>&#40;</td>
-                            <td onClick={(event)=>setData(data + event.target.innerText) } className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] w-16'>&#41;</td>
-                            <td onClick={(event)=>setData(data + '%') } className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8]'>mod</td>
-                            <td onClick={(event)=>setData(data + event.target.innerText) } className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8]'>&#xF7;</td>
-                        </tr>
+                        <div className=''>
+                            <button onClick={(event)=>setData(data + event.target.innerText) }  className='m-2 h-14 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl w-16 '>&#40;</button>
+                            <button onClick={(event)=>setData(data + event.target.innerText) } className='m-2 h-14 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl w-16'>&#41;</button>
+                            <button onClick={(event)=>setData(data + '%') } className='m-2 h-14 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl w-16'>mod</button>
+                            <button onClick={(event)=>setData(data + event.target.innerText) } className='m-2 h-14 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl w-16'>&#xF7;</button>
+                        </div>
 
-                        <tr className='text-center'>
-                            <td onClick={(event)=>setData(data + event.target.innerText)} className='cursor-pointer border-2 border-white hover:bg-[#e5faf8]'>7</td>
-                            <td onClick={(event)=>setData(data + event.target.innerText)} className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8]'>8</td>
-                            <td onClick={(event)=>setData(data + event.target.innerText)} className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8]'>9</td>
-                            <td onClick={(event)=>setData(data + event.target.innerText)} className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8]'>&#215;</td>
-                        </tr>
+                        <div className=''>
+                            <button onClick={(event)=>setData(data + event.target.innerText)} className='m-2 h-14 w-16 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl'>7</button>
+                            <button onClick={(event)=>setData(data + event.target.innerText)} className='m-2 h-14 w-16 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl'>8</button>
+                            <button onClick={(event)=>setData(data + event.target.innerText)} className='m-2 h-14 w-16 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl'>9</button>
+                            <button onClick={(event)=>setData(data + event.target.innerText)} className='m-2 h-14 w-16 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl'>&#215;</button>
+                        </div>
 
-                        <tr className='text-center'>
-                            <td onClick={(event)=>setData(data + event.target.innerText)} className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8]'>4</td>
-                            <td onClick={(event)=>setData(data + event.target.innerText)} className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8]'>5</td>
-                            <td onClick={(event)=>setData(data + event.target.innerText)} className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8]'>6</td>
-                            <td onClick={(event)=>setData(data + event.target.innerText)} className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8]'>&#8722;</td>
-                        </tr>
+                        <div className='text-center'>
+                            <button onClick={(event)=>setData(data + event.target.innerText)} className='m-2 h-14 w-16 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl'>4</button>
+                            <button onClick={(event)=>setData(data + event.target.innerText)} className='m-2 h-14 w-16 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl'>5</button>
+                            <button onClick={(event)=>setData(data + event.target.innerText)} className='m-2 h-14 w-16 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl'>6</button>
+                            <button onClick={(event)=>setData(data + event.target.innerText)} className='m-2 h-14 w-16 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl'>&#8722;</button>
+                        </div>
 
-                        <tr className='text-center'>
-                            <td onClick={(event)=>setData(data + event.target.innerText)} className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8]'>1</td>
-                            <td onClick={(event)=>setData(data + event.target.innerText)} className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8]'>2</td>
-                            <td onClick={(event)=>setData(data + event.target.innerText)} className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8]'>3</td>
-                            <td onClick={(event)=>setData(data + event.target.innerText)} className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8]'>&#43;</td>
-                        </tr>
+                        <div className='text-center'>
+                            <button onClick={(event)=>setData(data + event.target.innerText)} className='m-2 h-14 w-16 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl'>1</button>
+                            <button onClick={(event)=>setData(data + event.target.innerText)} className='m-2 h-14 w-16 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl'>2</button>
+                            <button onClick={(event)=>setData(data + event.target.innerText)} className='m-2 h-14 w-16 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl'>3</button>
+                            <button onClick={(event)=>setData(data + event.target.innerText)} className='m-2 h-14 w-16 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl'>&#43;</button>
+                        </div>
 
-                        <tr className='text-center'>
-                            <td onClick={(event)=>setData(data + '.')} className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8]'>&#183;</td>
-                            <td onClick={(event)=>setData(data + event.target.innerText)} className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8]'>0</td>
-                            <td onClick={getParcentage} className='cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8]'>&#37; </td>
-                            <td onClick={()=>getTotal(data)} className='cursor-pointer border-2 border-[#e1feff] bg-cyan-300 hover:bg-cyan-400'>&#61;</td>
-                        </tr>
-                    </tbody>
-                </table>
+                        <div className='text-center'>
+                            <button onClick={(event)=>setData(data + '.')} className='m-2 h-14 w-16 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl'>&#183;</button>
+                            <button onClick={(event)=>setData(data + event.target.innerText)} className='m-2 h-14 w-16 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl'>0</button>
+                            <button onClick={getParcentage} className='m-2 h-14 w-16 cursor-pointer border-2 border-[#e1feff] hover:bg-[#e5faf8] rounded-xl'>&#37; </button>
+                            <button onClick={()=>getTotal(data)} className='bg-[#efa98f] hover:bg-[#e89c80] m-2 h-14 w-16 cursor-pointer border-2 border-[#e1feff] rounded-xl'>&#61;</button>
+                        </div>
+                    </div>
+            </div>
         </div>
     )
 }
